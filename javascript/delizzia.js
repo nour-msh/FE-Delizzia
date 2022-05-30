3240a46f7b69621c394624a2b0ac07bf2fed18fd
Nour Mshawrab <mshawrabnour@gmail.com>
2022-05-30 11:25:15 +0300

    Implementing a popup for profile editing

---
+++ javascript/delizzia.js
@@ -0,0 +1,12 @@
+$("#profileImage").click(function(e) {
+    $("#imageUpload").click();
+});
+
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
