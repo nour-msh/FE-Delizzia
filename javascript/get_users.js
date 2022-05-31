ee9287b72600024bf71c69d3bdafefe69aa485c9
Nour Mshawrab <mshawrabnour@gmail.com>
2022-05-31 18:02:26 +0300

    Implementing get users page

---
+++ javascript/get_users.js
@@ -0,0 +1,13 @@
+const users = document.getElementById("users");
+users.addEventListener("click", function(){
+  let data = new FormData();
+      axios({
+          method: 'get',
+          url: 'http://Localhost/delizzia/APIs/get_users.php',
+          data: data,
+      })
+      .then(function (response) {
+          console.log(response);
+          }
+      )
+});
