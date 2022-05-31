2111f33b1a60bec3db6067a6010d0e60665b2e43
Nour Mshawrab <mshawrabnour@gmail.com>
2022-05-31 18:03:28 +0300

    Implementing editing profile page

---
+++ javascript/edit_profile.js
@@ -0,0 +1,35 @@
+const profButton = document.getElementById("profButton");
+const hide= document.getElementById("closeBtn");
+const saveBtn= document.getElementById("saveBtn");
+const fname= document.getElementById("fname");
+const lname= document.getElementById("lname");
+const email= document.getElementById("email");
+const password= document.getElementById("password");
+const number= document.getElementById("number");
+
+
+profButton.addEventListener("click", openForm());
+    function openForm(){
+        document.getElementById("myForm").style.display = "block";
+    }
+
+hide.addEventListener("click", closeForm());
+    function closeForm(){
+        document.getElementById("myForm").style.display = "none";
+    }
+saveBtn.addEventListener("click", function(){
+    let data = new FormData();
+        data.append('firstname', fname.value);
+        data.append('lastname', lname.value);
+        data.append('email', email.value);
+        data.append('password', password.value);
+        data.append('number', number.value);
+        axios({
+          method: 'post',
+          url: 'http://Localhost/delizzia/APIs/profile.php',
+          data: data,
+      })
+        .then(function (response) {
+          console.log(response);
+          }
+      )})
