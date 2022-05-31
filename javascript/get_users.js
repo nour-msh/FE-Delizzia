a27d2f2fc851025ab0beb41949875db09f63c9ff
Nour Mshawrab <mshawrabnour@gmail.com>
2022-05-31 22:38:09 +0300

    Adding uers to users page

--- javascript/get_users.js
+++ javascript/get_users.js
@@ -1,14 +1,41 @@
 const users = document.getElementById("users");
-users.addEventListener("click", function(){
+users.addEventListener("click", function () {
   let data = new FormData();
-      axios({
-          method: 'get',
-          url: 'http://Localhost/delizzia/APIs/get_users.php',
-          data: data,
-      })
-      .then(function (response) {
-          console.log(response);
-          window.location.href="users.html"
-          }
-      )
+  axios({
+    method: "get",
+    url: "http://Localhost/delizzia/APIs/get_users.php",
+    data: data,
+  }).then(function (res) {
+    console.log(res.data);
+    localStorage.setItem("users", JSON.stringify(res.data));
+    window.location.href = "users.html";
+  });
 });
+
+// const userFromlocal = localStorage.getItem("users");
+// const allUsers = JSON.parse(userFromlocal);
+// console.log(allUsers);
+// var ul = document.createElement("ul");
+// ul.setAttribute("id", "ulID");
+// document.body.appendChild(ul);
+
+// for (var i = 0; i < allUsers.length; i++) {
+//   var li = document.createElement("li");
+//   d1 = document.createDocumentFragment();
+//   d1.appendChild(li);
+//   var text1 = document.createTextNode(allUsers[i].firstname + " ");
+//   var text2 = document.createTextNode(allUsers[i].lastname + " ");
+//   var text3 = document.createTextNode(allUsers[i].email + " ");
+//   var text4 = document.createTextNode(allUsers[i].number);
+//   li.appendChild(text1);
+//   li.appendChild(text2);
+//   li.appendChild(text3);
+//   li.appendChild(text4);
+  
+//   elm = document.getElementById("ul");
+//   elm.appendChild(d1);
+
+// //   ul.appendChild(li);
+// }
+// var element = document.getElementById("new");
+// element.appendChild(ul);
