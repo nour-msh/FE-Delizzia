c5827486f8cd5821584b2b879a3835bbc9ed9617
Nour Mshawrab <mshawrabnour@gmail.com>
2022-05-31 18:01:45 +0300

    Finalising login page

---
+++ javascript/login.js
@@ -0,0 +1,26 @@
+
+const btn = document.getElementById("loginButton");
+const email= document.getElementById("email");
+const password= document.getElementById("password");
+btn.addEventListener("click", function(){
+    let data = new FormData();
+        data.append('email', email.value);
+        data.append('password', password.value);
+        axios({
+            method: 'post',
+            url: 'http://Localhost/delizzia/APIs/login.php',
+            data: data,
+        })
+        .then(function (response) {
+            console.log(response);
+            let result=response.data;
+            if (result.response=="Logged in"){
+                if(email.value =="admin@gmail.com" & password.value == "pizza_admin"){
+                    window.location.href="file:///C:/Users/user/Desktop/frontEnd_Delizzia/adminPage.html"
+                }
+                else{
+                console.log("hi");
+                window.location.href="file:///C:/Users/user/Desktop/frontEnd_Delizzia/profile.html";
+            }
+            }})
+        });
